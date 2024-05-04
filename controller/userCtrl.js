10a96408af4edbf9e6ee7979a54c28ed51d152d1
Israel Ogunsola <110337578+Issybobo@users.noreply.github.com>
2024-05-04 16:45:17 +0100

    added few controllers

--- controller/userCtrl.js
+++ controller/userCtrl.js
@@ -1,5 +1,6 @@
 const User = require("../models/userModel");
-const asyncHandler = require("express-async-handler")
+const asyncHandler = require("express-async-handler");
+const {generateToken} = require("../config/jwtToken");
 
 // Create a User ----------------------------------------------
 
@@ -26,5 +27,65 @@ const createUser = (async (req, res) => {
       throw new Error("User Already Exists");
     }
   });
+
+  // LOGIN A USER
+  const loginUserCtrl = asyncHandler(async (req, res) => {
+    const { email, password } = req.body;
+    // check if user exists or not
+    const findUser = await User.findOne({ email });
+    if (findUser && (await findUser.isPasswordMatched(password))) {
+      /*const refreshToken = await generateRefreshToken(findUser?._id);
+      const updateuser = await User.findByIdAndUpdate(
+        findUser.id,
+        {
+         refreshToken: refreshToken,
+        },
+        { new: true }
+      );
+      res.cookie("refreshToken", refreshToken, {
+        httpOnly: true,
+        maxAge: 72 * 60 * 60 * 1000,
+      });*/
+      res.json({
+        _id: findUser?._id,
+        firstname: findUser?.firstname,
+        lastname: findUser?.lastname,
+        email: findUser?.email,
+        mobile: findUser?.mobile,
+        token: generateToken(findUser?._id),
+      });
+    } else {
+      throw new Error("Invalid Credentials");
+    }
+  });
+
+  // GET ALL USERS
+
+
+const getallUser = asyncHandler(async (req, res) => {
+  try {
+    const getUsers = await User.find();//opulate("wishlist");
+    res.json(getUsers);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+// Get a single user
+
+const getaUser = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+ // validateMongoDbId(id);
+
+  try {
+    const getaUser = await User.findById(id);
+    res.json({
+      getaUser,
+    });
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
   
-  module.exports  = {createUser} 
+  module.exports  = {createUser, loginUserCtrl, getallUser, getaUser};
