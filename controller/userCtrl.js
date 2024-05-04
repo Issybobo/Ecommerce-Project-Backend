37408c8643433408b238664735594cdac5db3e47
Israel Ogunsola <110337578+Issybobo@users.noreply.github.com>
2024-05-04 02:29:03 +0100

    added the middleware

--- controller/userCtrl.js
+++ controller/userCtrl.js
@@ -1,4 +1,5 @@
 const User = require("../models/userModel");
+const asyncHandler = require("express-async-handler")
 
 // Create a User ----------------------------------------------
 
--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const PORT = process.env.PORT || 4000;
 const dbConnect = require("./config/dbConnect");
 const authRouter = require("./routes/authRouter");
 const bodyParser = require('body-parser');
+const { notFound, errorHandler } = require('./middlewares/errorHandler');
 
 
 // Call dbConnect to establish MongoDB connection
@@ -18,6 +19,10 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.use("/api/user", authRouter);
+app.use(notFound)
+app.use(errorHandler)
+
+
 
 app.listen(PORT, () => {
     console.log(`Server is running at PORT ${PORT}`);
